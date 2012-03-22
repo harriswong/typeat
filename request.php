f03af09bf07ac786b871eb52f30fdcb469cdff66
Harris Wong <harris.wong@gmail.com>
2012-03-22 16:56:44 -0400

    Added basic files
    Added basic template and request script to handle dropdown request.  Uses jquery ui for dropdown.  Problem with binding events within a binder.

---
+++ request.php
@@ -0,0 +1,20 @@
+<?php
+	$friends = array();
+	$returnFriends = array();
+	$_GET['term'] = urldecode($_GET['term']);
+	
+	if ($_GET['term']) {
+		$friends = array (
+			'@h1' => 'harris1', 
+			'@h2' => 'harris2', 
+			'@h3' => 'harris3'
+		);
+		foreach ($friends as $k => $v) {
+			if (strpos($k, $_GET['term']) === 0) {
+				$returnFriends[] = $v;
+			}
+		}
+	}
+	echo json_encode($returnFriends);
+	exit;
+?>

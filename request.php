05effaaafea7e7560240ba4d20ff090169f8aca1
Harris Wong <harris.wong@gmail.com>
2012-03-23 10:19:23 -0400

    Accept any @string in the text area
    Accept any string in the textarea, and as soon as there is an "@" sign read, it will start returning an autocomplete list using jquery autocomplete plugin .

--- request.php
+++ request.php
@@ -1,13 +1,13 @@
 <?php
 	$friends = array();
 	$returnFriends = array();
-	$_GET['term'] = urldecode($_GET['term']);
+	$_GET['term'] = urldecode(strtolower($_GET['term']));
 	
 	if ($_GET['term']) {
 		$friends = array (
-			'@h1' => 'harris1', 
-			'@h2' => 'harris2', 
-			'@h3' => 'harris3'
+			'h1' => 'harris1', 
+			'h2' => 'harris2', 
+			'h3' => 'harris3'
 		);
 		foreach ($friends as $k => $v) {
 			if (strpos($k, $_GET['term']) === 0) {
